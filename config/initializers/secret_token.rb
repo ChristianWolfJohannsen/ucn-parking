c9ef9d6fbd6612d0dadd3d2618af6f898c4a0fa5
Christian Høj <cbh@ucn.dk>
2013-10-31 16:42:54 +0100

    Fix deprecation message

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Ucnparking::Application.config.secret_token = '8455858d1ed6ad1a2aa0d781d9b3c8e25ebd48628a493387cc39db055847a04d51693a4a136a957fa473b768d407f5a1351d2f4efd655c6e3f6586de2d3f9020'
+Ucnparking::Application.config.secret_key_base = '798178bc00d46859c9cdf25eff9cfbca568e87c3dbe388da0e4ee257cbfbbd47afff400151a55bfdf8b9e46894fd1f3803df122a6248e041e9d67990a5e05b6c'
