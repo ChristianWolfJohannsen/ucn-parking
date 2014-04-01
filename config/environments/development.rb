aef30b9f524393e14aa5803f0ab567fa9a67c10e
Christian Høj <cbh@ucn.dk>
2014-04-02 01:20:23 +0200

    Change cache time out to 2 minutes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Ucnparking::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.cache_store = :memory_store, { expires_in: 300, race_condition_ttl: 300 }
+  config.cache_store = :memory_store, { expires_in: 2.minutes, race_condition_ttl: 2.minutes }
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
